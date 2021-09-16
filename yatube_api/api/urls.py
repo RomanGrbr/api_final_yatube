09e1d8e95760ea22612100b4b161bae4c871e6ba
Роман <romangrbr@gmail.com>
2021-09-16 22:48:39 +0300

    14 failed, 18 passed, 10 warnings

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,19 +1,16 @@
 from django.urls import path, include
 from rest_framework import routers
-from rest_framework import permissions
 
-from django.conf.urls import url
+from api.views import PostViewSet, CommentsViewSet, FollowViewSet, GroupViewSet
 
-from api.views import PostViewSet, CommentsViewSet, FollowViewSet
 
 router = routers.DefaultRouter()
-router.register(r'posts', PostViewSet)
-router.register(r'comments', CommentsViewSet)
+router.register(r'posts', PostViewSet, basename='posts')
 router.register(
     r'posts/(?P<id>\d+)/comments', CommentsViewSet, basename='comments'
 )
-router.register(r'follow', FollowViewSet)
-router.register(r'groups', FollowViewSet)
+router.register(r'follow', FollowViewSet, basename='follows')
+router.register(r'groups', GroupViewSet, basename='groups')
 
 urlpatterns = [
     path('v1/', include(router.urls)),
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,9 +1,12 @@
 from rest_framework import viewsets
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.generics import get_object_or_404
 
 from posts.models import Post, Comment, Follow, Group
 from .serializers import PostSerializer, CommentSerializer, FollowSerializer, \
     GroupSerializer
 from rest_framework.pagination import LimitOffsetPagination
+from .permissions import UserOrReadOnly
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -11,10 +14,44 @@ class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
     pagination_class = LimitOffsetPagination
 
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
 
-class CommentsViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Comment.objects.all()
+    def perform_update(self, serializer):
+        if serializer.instance.author != self.request.user:
+            raise PermissionDenied('Изменение чужого контента запрещено!')
+        super().perform_update(serializer)
+
+    def perform_destroy(self, instance):
+        if instance.author != self.request.user:
+            raise PermissionDenied('Изменение чужого контента запрещено!')
+        super().perform_destroy(instance)
+
+
+class CommentsViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
+    permission_classes = (UserOrReadOnly,)
+
+    def perform_create(self, serializer):
+        post = get_object_or_404(Post, id=self.kwargs.get("id"))
+        serializer.save(
+            author=self.request.user, post=post
+        )
+
+    def get_queryset(self):
+        post = get_object_or_404(Post, id=self.kwargs.get("id"))
+        new_queryset = Comment.objects.filter(post=post)
+        return new_queryset
+
+    def perform_update(self, serializer):
+        if serializer.instance.author != self.request.user:
+            raise PermissionDenied('Изменение чужого контента запрещено!')
+        super().perform_update(serializer)
+
+    def perform_destroy(self, instance):
+        if instance.author != self.request.user:
+            raise PermissionDenied('Изменение чужого контента запрещено!')
+        super().perform_destroy(instance)
 
 
 class FollowViewSet(viewsets.ModelViewSet):
